fb8f9977a45b3066475d093d6f99f8a2d11f0f4a
cannadayr <cannadayr@gmail.com>
2019-02-27 21:57:00 +0000

    Update disk_part.sh

--- disk_part.sh
+++ disk_part.sh
@@ -19,16 +19,3 @@ unit: sectors
 /dev/sda2 : start=     1050624, size=    33554432, type=82
 /dev/sda3 : start=    34605056, size=   465513136, type=bf
 EOF
-
-# create zfs partitions
-#TODO add encryption
-zpool create -O canmount=off -O mountpoint=/ -R /mnt/ rpool $wwn-part3
-zfs create -o canmount=off -o mountpoint=none rpool/ROOT
-zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/debian
-zfs mount rpool/ROOT/debian
-zpool set bootfs=rpool/ROOT/debian rpool
-
-# encrypt swap #TODO add back in
-#cryptsetup -y -v luksFormat /dev/sda2
-#cryptsetup luksOpen /dev/disk/by-id/wwn-[SDA_ID]-part2 luks-swap 
-#mkswap -f /dev/mapper/luks-swap
