fe789870aec7389e0a994813b749b4e5fcaedc76
cannadayr <cannadayr@gmail.com>
2019-02-27 23:51:10 +0000

    Update chroot_build.sh

--- chroot_build.sh
+++ chroot_build.sh
@@ -16,6 +16,7 @@ apt-get update
 apt-get install -y locales
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
+passwd
 
 apt-get install -y build-essential autoconf libtool gawk alien fakeroot \
   zlib1g-dev uuid-dev libattr1-dev libblkid-dev libselinux-dev libudev-dev \
