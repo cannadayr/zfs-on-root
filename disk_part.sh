921e6b4fe2ba60248ad2dec569a260f3099d73d1
cannadayr <cannadayr@gmail.com>
2019-02-27 21:11:35 +0000

    Update disk_part.sh

--- disk_part.sh
+++ disk_part.sh
@@ -5,22 +5,19 @@ wwn=$(ls -1 /dev/disk/by-id/wwn-* | grep -v "\-part[0-9]$")
 sgdisk --zap-all $wwn
 sgdisk --zap-all $wwn
 
-# create boot partition
-sgdisk -n1:0:0 -t1:BF01 $wwn
+sfdisk /dev/sda <<EOF
+Disk /dev/sda: 238.5 GiB, 256060514304 bytes, 500118192 sectors
+Units: sectors of 1 * 512 = 512 bytes
+Sector size (logical/physical): 512 bytes / 4096 bytes
+I/O size (minimum/optimal): 4096 bytes / 4096 bytes
+Disklabel type: dos
+Disk identifier: 0x4d2465be
 
-
-#TODO script here
-
-#Units: sectors of 1 * 512 = 512 bytes
-#Sector size (logical/physical): 512 bytes / 4096 bytes
-#I/O size (minimum/optimal): 4096 bytes / 4096 bytes
-#Disklabel type: dos
-#Disk identifier: [id]
-#
-#Device     Boot    Start       End   Sectors  Size Id Type
-#/dev/sda1           2048   1050623   1048576  512M  c W95 FAT32 (LBA)
-#/dev/sda2        1050624  34605055  33554432   16G 82 Linux swap / Solaris
-#/dev/sda3       34605056 500118191 465513136  222G bf Solaris
+Device     Boot    Start       End   Sectors  Size Id Type
+/dev/sda1           2048   1050623   1048576  512M  c W95 FAT32 (LBA)
+/dev/sda2        1050624  34605055  33554432   16G 82 Linux swap / Solaris
+/dev/sda3       34605056 500118191 465513136  222G bf Solaris
+EOF
 
 # encrypt swap #TODO add back in
 #cryptsetup -y -v luksFormat /dev/sda2
@@ -29,7 +26,7 @@ sgdisk -n1:0:0 -t1:BF01 $wwn
 
 # add zfs pool + partition
 #TODO add native encryption
-zpool create -O mountpoint=/ -R /mnt rpool /dev/disk/by-id/wwn-[ID]-part3
-zfs create -o mountpoint=none rpool/ROOT
-zfs create -o mountpoint=/ rpool/ROOT/debian
-zpool set bootfs=rpool/ROOT/debian rpool
+#zpool create -O mountpoint=/ -R /mnt rpool /dev/disk/by-id/wwn-[ID]-part3
+#zfs create -o mountpoint=none rpool/ROOT
+#zfs create -o mountpoint=/ rpool/ROOT/debian
+#zpool set bootfs=rpool/ROOT/debian rpool
