087c6d091326115e21784df289794ae1189a7469
cannadayr <cannadayr@gmail.com>
2019-02-27 23:07:43 +0000

    Update zfs_install.sh

--- zfs_install.sh
+++ zfs_install.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get install -y build-essential autoconf libtool gawk alien fakeroot \
   zlib1g-dev uuid-dev libattr1-dev libblkid-dev libselinux-dev libudev-dev \
   parted lsscsi ksh libssl-dev libelf-dev linux-headers-$(uname -r) \
-  git gdebi python3-dev python3-setuptools python3-cffi cryptsetup debootstrap
+  git gdebi-core python3-dev python3-setuptools python3-cffi cryptsetup debootstrap
 
 # clone & build zfs
 git clone --depth=1 https://github.com/zfsonlinux/zfs
