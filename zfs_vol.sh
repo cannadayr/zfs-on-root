fba946e0fb6c18570e9d61ccbf9835c6deffc879
cannadayr <cannadayr@gmail.com>
2019-02-27 23:48:35 +0000

    Update zfs_vol.sh

--- zfs_vol.sh
+++ zfs_vol.sh
@@ -5,8 +5,12 @@ wwn=$(ls -1 /dev/disk/by-id/wwn-* | grep -v "\-part[0-9]$")
 zpool create -O canmount=off -O mountpoint=/ -R /mnt/ rpool $wwn-part3
 zfs create -o canmount=off -o mountpoint=none rpool/ROOT
 zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/debian
-zfs mount rpool/ROOT/debian
 zpool set bootfs=rpool/ROOT/debian rpool
+
+# mount stuff
+zfs mount rpool/ROOT/debian
+mkdir /mnt/boot
+mount $wwn-part1 /mnt/boot
 
 # encrypt swap #TODO add back in
 #cryptsetup -y -v luksFormat /dev/sda2
