963dfb17f22a3efe09788b67187e56bb4c12a12c
cannadayr <cannadayr@gmail.com>
2019-02-27 22:59:09 +0000

    Update chroot_build.sh

--- chroot_build.sh
+++ chroot_build.sh
@@ -3,8 +3,25 @@ debootstrap stretch /mnt/
 
 # configure os
 cp /etc/apt/sources.list.d/base.list /mnt/etc/apt/sources.list
+echo vega > /mnt/etc/hostname
+sed -i '1 s/$/ vega/' /mnt/etc/hosts
 
-# mount pseudo filesystems & chroot into it
+mount --rbind /dev  /mnt/dev
+mount --rbind /proc /mnt/proc
+mount --rbind /sys  /mnt/sys
+chroot /mnt /bin/bash --login
+
+ln -s /proc/self/mounts /etc/mtab
+apt-get update
+apt-get install -y locales
+dpkg-reconfigure locales
+
+apt-get install -y build-essential autoconf libtool gawk alien fakeroot \
+  zlib1g-dev uuid-dev libattr1-dev libblkid-dev libselinux-dev libudev-dev \
+  parted lsscsi ksh libssl-dev libelf-dev linux-headers-$(uname -r) \
+  git gdebi python3-dev python3-setuptools python3-cffi cryptsetup
+
+# mount pseudo filesystems & chroot into i
 # install zfs build dependencies
 # install locale
 # dpkg-reconfigure locale
