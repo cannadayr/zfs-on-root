c253bed333727acf0fc18f69dc1707860e87ce34
cannadayr <cannadayr@gmail.com>
2019-02-27 20:16:04 +0000

    Update setup.sh
    sketching out steps & commenting

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ for file in *.deb; do gdebi -q --non-interactive $file; done
 modprobe zfs
 
 # partition ssd
+#TODO script here
 #Units: sectors of 1 * 512 = 512 bytes
 #Sector size (logical/physical): 512 bytes / 4096 bytes
 #I/O size (minimum/optimal): 4096 bytes / 4096 bytes
@@ -32,12 +33,13 @@ modprobe zfs
 #/dev/sda2        1050624  34605055  33554432   16G 82 Linux swap / Solaris
 #/dev/sda3       34605056 500118191 465513136  222G bf Solaris
 
-# encrypt swap
-cryptsetup -y -v luksFormat /dev/sda2
-cryptsetup luksOpen /dev/disk/by-id/wwn-[SDA_ID]-part2 luks-swap 
-mkswap -f /dev/mapper/luks-swap
+# encrypt swap #TODO add back in
+#cryptsetup -y -v luksFormat /dev/sda2
+#cryptsetup luksOpen /dev/disk/by-id/wwn-[SDA_ID]-part2 luks-swap 
+#mkswap -f /dev/mapper/luks-swap
 
 # add zfs pool + partition
+#TODO add native encryption
 zpool create -O mountpoint=/ -R /mnt rpool /dev/disk/by-id/wwn-[ID]-part3
 zfs create -o mountpoint=none rpool/ROOT
 zfs create -o mountpoint=/ rpool/ROOT/debian
@@ -45,3 +47,15 @@ zpool set bootfs=rpool/ROOT/debian rpool
 
 # install OS
 debootstrap stretch /mnt/
+
+# configure os
+cp /etc/apt/sources.list.d/base.list /mnt/etc/apt/sources.list
+
+# mount pseudo filesystems & chroot into it
+# install zfs build dependencies
+# set locale
+# build + install zfs
+# install bootloader
+# exit
+# umount
+# reboot
